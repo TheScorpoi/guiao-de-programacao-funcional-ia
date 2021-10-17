4b4ecac1b4999e3649410260ca5ffbfca77bedf9
TheScorpoi <sobral@ua.pt>
2021-10-17 02:42:15 +0100

    all exercices 1 done

--- aula1.py
+++ aula1.py
@@ -1,38 +1,78 @@
 #Exercicio 1.1
 def comprimento(lista):
-	pass
+    if lista == []:
+        return 0
+    return 1 + comprimento(lista[1:])
 
 #Exercicio 1.2
 def soma(lista):
-	pass
+    if lista == []:
+        return 0
+    return lista[0] + soma(lista[1:])
 
 #Exercicio 1.3
 def existe(lista, elem):
-	pass
+	if lista == []:
+		return False
+	if lista[0] == elem:
+		return True
+	return existe(lista[1:], elem)
+
 
 #Exercicio 1.4
 def concat(l1, l2):
-	pass
+
+    
+    if l1 == []:
+        return l2
+    if l2 == []:
+        return l1
+    l1.append(l2[0])
+    return concat(l1, l2[1:])
 
 #Exercicio 1.5
 def inverte(lista):
-	pass
+	if lista == []:
+		return []
+	return [lista[-1]] + inverte(lista[0:-1])
 
 #Exercicio 1.6
 def capicua(lista):
-	pass
+    if lista == []:
+        return True
+    if lista[0] == lista[-1]:
+        return capicua(lista[1:-1])
+    else:
+        return False
 
 #Exercicio 1.7
 def explode(lista):
-	pass
+    if lista == []:
+        return []
+    return lista[0] + explode(lista[1:]) 
+    
 
 #Exercicio 1.8
 def substitui(lista, original, novo):
-	pass
-
+    if lista == []:
+        return []
+    if lista[0] == original:
+        lista[0] = novo
+    return [lista[0]] + substitui(lista[1:], original, novo)
+    
 #Exercicio 1.9
 def junta_ordenado(lista1, lista2):
-	pass
+    if lista1 == [] and lista2 != []:
+        return lista2
+    elif lista1 != [] and lista2 == []:
+        return lista1
+    elif lista1 == [] and lista2 == []:
+        return []
+    else:
+        if lista1[0] < lista2[0]:
+            return [lista1[-1]] + [lista2[-1]] + junta_ordenado(lista1[:-1], lista2[:-1])
+        else:
+            return [lista2[-1]] + [lista1[-1]] + junta_ordenado(lista1[:-1], lista2[:-1])
 
 #Exercicio 2.1
 def separar(lista):
@@ -61,3 +101,25 @@ def menor(lista):
 #Exercicio 3.6
 def max_min(lista):
 	pass
+
+if __name__ == '__main__':
+    lista = [1,2,2,1]
+    
+    print("Exercicio 1.1: ", comprimento(lista))
+    print("Exercicio 1.2: ", soma(lista))
+    print("Exercicio 1.3: ", existe(lista, 2))
+    print("Exercicio 1.4: ", concat(lista, lista))
+    print("Exercicio 1.5: ", inverte(lista))
+    print("Exercicio 1.6: ", capicua(lista))
+    print("Exercicio 1.7: ", explode([lista,lista, lista]))
+    print("Exercicio 1.8: ", substitui(lista, 1, 2))
+    print("Exercicio 1.9: ", junta_ordenado(lista, lista))
+
+    #print("Exercicio 2.1: ", separar(lista))
+    #print("Exercicio 2.2: ", remove_e_conta(lista, 2))
+
+    #print("Exercicio 3.1: ", cabeça(lista))
+    #print("Exercicio 3.2: ", cauda(lista))
+    #print("Exercicio 3.3: ", juntar(lista, lista))
+    #print("Exercicio 3.4: ", menor(lista))
+    #print("Exercicio 3.6: ", max_min(lista))
